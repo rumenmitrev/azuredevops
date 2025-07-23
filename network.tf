a956b6fb528f532d2f7f3a445179e0342a4f8398
rumen.mitrev <rumen.mitrev@sirma.com>
2025-07-23 17:45:28 +0300

    share .terraform

--- network.tf
+++ network.tf
@@ -8,6 +8,6 @@ resource "azurerm_virtual_network" "example" {
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
   resource_group_name  = azurerm_resource_group.rumen-resource-group.name
-  virtual_network_name = var.location
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["172.16.1.0/24"]
 }
